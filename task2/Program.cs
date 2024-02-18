fc8b580c5bcf105309a23eff44c6383f1dd5b023
SERGMOLOTKOV <ccmolotkov@gmail.com>
2024-02-18 19:40:50 +0300

    task2 add

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = "HeLLo WorLD";
+        string resultString = ConvertToUpperToLower(inputString);
+        
+        Console.WriteLine("Результирующая строка: " + resultString);
+    }
+
+    static string ConvertToUpperToLower(string input)
+    {
+        // Создаем пустую строку для результата
+        string result = "";
+
+        // Обходим каждый символ входной строки
+        foreach (char c in input)
+        {
+            // Проверяем, является ли символ заглавной буквой
+            if (char.IsUpper(c))
+            {
+                // Если да, добавляем его же в нижнем регистре к результату
+                result += char.ToLower(c);
+            }
+            else
+            {
+                // Если это не заглавная буква, добавляем его без изменений
+                result += c;
+            }
+        }
+
+        return result;
+    }
+}
